f49a09c9f815334a680e58f3d9503bc2c3a78541
shinolajla <shinolajla@f793d0a6-cd2d-0410-9c2f-63c70812b45e>
2007-05-30 01:17:12 +0000

    Added missing interfaces ModelComment and SiteUser.

---
+++ trunk/ahpfree/src/com/ahpfree/ahp/model/interfaces/ModelComment.java
@@ -0,0 +1,17 @@
+/*
+ * ModelComment.java
+ * 
+ * Created on May 29, 2007, 7:17:49 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package com.ahpfree.ahp.model.interfaces;
+
+/**
+ *
+ * @author Jamie
+ */
+public interface ModelComment {
+
+}
